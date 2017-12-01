818fd6547c0481011011bda98924420e5bc3b109
Sean Yalda <seanyalda@gmail.com>
2017-12-01 01:23:02 -0800

    latest update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 	var adCount = 0;
 	var wHeight = $(window).height();
@@ -97,9 +98,61 @@ $(document).ready(function(){
 			$('body').toggleClass('noscroll');
 	});
 
-	$('.gala-window').find('.close').on('click', function() {
+	$('.gala-container').children('.close').on('click', function() {
 			$('.gala-window').removeClass('show');
 			$('body').removeClass('noscroll');
+	});
+
+	$('.mediabox').on('click', function() {
+			var d = $(this).data("gallery-item");
+			$('.gala-gallery').addClass('show');
+			$('.gala-media-' + d).addClass('show');
+			$('.gala-window').addClass('noscroll');
+			$('.gui').addClass('hide');
+	});
+
+	$('.gala-gallery').find('.controls').find('.close').on('click', function() {
+		$('.gala-window').removeClass('noscroll');
+		$('.gala-gallery').removeClass('show');
+		$('.gala-media.show').removeClass('show')
+		$('.gui').removeClass('hide');
+	});
+
+	$('.gala-gallery').find('.controls').find('.navbutton').on('click', function() {
+		var itemClass = $('.gala-media.show').attr('class').split(' ')[1];
+		var item = itemClass.split('-');
+		var itemName = item[item.length-1];
+		var itemNumber = Number(itemName.split("item")[1]);
+		var mediaLength = $('.gala-media').length;
+		if($(this).hasClass('next')) {
+			if( itemNumber < mediaLength ) {
+				$("." + itemClass).removeClass('show');
+				$("." + itemClass).next().addClass('show');
+			} else {
+				$("." + itemClass).removeClass('show');
+				$(".gala-media-item1").addClass('show');
+			}
+		} else {
+			if( itemNumber > 1 ) {
+				$("." + itemClass).removeClass('show');
+				$("." + itemClass).prev().addClass('show');
+			} else {
+				$("." + itemClass).removeClass('show');
+				$(".gala-media-item" + mediaLength).addClass('show');
+			}
+		}
+	});
+
+	$('#playintro').on('click', function() {
+			$('.video-modal').toggleClass('show');
+			$(".video-modal").find("iframe")[0].src += "&autoplay=1";
+			$('body').toggleClass('noscroll');
+	});
+
+	$('.video-modal').find('.close').on('click', function() {
+			$('.video-modal').toggleClass('show');
+			$('body').removeClass('noscroll');
+			$(".video-modal").find('iframe').attr("src", $(".video-modal").find('iframe').attr("src").split("&")[0]);
 	});
 
 	// $('.ad-modal').find('.close').on('click', function() {
@@ -109,6 +162,7 @@ $(document).ready(function(){
 	// 	}
 	// 	$('body').toggleClass('noscroll');
 	// });
+
 
 
 });
